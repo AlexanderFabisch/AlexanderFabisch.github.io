d1a27f5610fdfb5135dad54658d15386916be067
Alexander Fabisch <afabisch@googlemail.com>
2021-03-25 20:02:05 +0100

    Switch to pelican-jupyter in conda environment

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+try:
+    # https://github.com/danielfrg/pelican-jupyter
+    from pelican_jupyter import markup as nb_markup
+except ImportError:
+    print("run: pip install pelican-jupyter")
+    exit(1)
 
 AUTHOR = 'Alexander Fabisch'
 SITENAME = 'Alexander Fabisch'
@@ -19,14 +25,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+MARKUP = ("md", "ipynb")
+
 THEME="pelican-themes/basic"
 PLUGIN_PATHS = ['pelican-plugins']
 STATIC_PATHS = ['images']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code', 'liquid_tags.notebook']
+           'liquid_tags.include_code', nb_markup]
+IPYNB_MARKUP_USE_FIRST_CELL = False
 with open('_nb_header.html', "r") as f:
     EXTRA_HEADER = f.read()
+IGNORE_FILES = [".ipynb_checkpoints"]
 
 # Blogroll
 LINKS = ()
