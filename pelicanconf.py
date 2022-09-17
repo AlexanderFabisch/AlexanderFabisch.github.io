5145d0c7d23ecee0dd90a901314207f20c54ff3e
Alexander Fabisch <afabisch@googlemail.com>
2022-09-18 00:24:46 +0200

    Fix pelican-jupyter

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 # https://github.com/danielfrg/pelican-jupyter
-from pelican_jupyter import liquid as nb_liquid
+from pelican_jupyter import markup as nb_markup
 
 AUTHOR = 'Alexander Fabisch'
 SITENAME = 'Alexander Fabisch'
@@ -26,9 +26,7 @@ MARKUP = ("md", "ipynb")
 THEME="pelican-themes/basic"
 PLUGIN_PATHS = ['pelican-plugins']
 STATIC_PATHS = ['images']
-PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
-           'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code', nb_liquid]
+PLUGINS = [nb_markup]
 IPYNB_MARKUP_USE_FIRST_CELL = False
 with open('_nb_header.html', "r") as f:
     EXTRA_HEADER = f.read()
